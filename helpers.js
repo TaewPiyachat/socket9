4cddc0e2d1cc05edaa6ee3e812abd8bd78e3d663
Piyachat Mongkolpatthanakul <taeaew.w@outlook.com>
2020-07-26 14:26:44 +0700

    fix valid year condition

--- helpers.js
+++ helpers.js
@@ -35,6 +35,6 @@ export const isValidMonth = month => {
 }
 
 export const isValidYear = year => {
-  return typeof year === 'number' && year > 0
+  return typeof year === 'number' && year >= 1900
 }
 
